7315ebd5d71d163c97e0b824a92879ccc91806aa
markelgomez-coder <markel.gomezr@gmail.com>
2026-04-17 12:35:18 +0200

    Test descripcion panel

--- test/panel-pokemon.test.js
+++ test/panel-pokemon.test.js
@@ -1,2 +1,35 @@
 import { describe, it, expect, beforeAll, beforeEach } from "vitest";
 import * as funcionesPanel from "../dist/ts/panel-pokemon.js";
+import * as funciones from "../dist/ts/funciones-generales.js";
+
+describe("El panel muestra el pokemon", () => {
+  it("Recoge bien el nombre del pokemon del url", () => {
+    const url = "http://localhost:3000/panel-pokemon.html?pokemon=bulbasaur";
+    const params = new URL(url).searchParams;
+    const pokemon = params.get("pokemon");
+
+    expect(pokemon).toBe("bulbasaur");
+  });
+
+  it("Recoge bien el numero del pokemon del url", () => {
+    const url = "http://localhost:3000/panel-pokemon.html?pokemon=1";
+
+    const params = new URL(url).searchParams;
+    const pokemon = { numero: params.get("pokemon"), nombre: "bulbasaur" };
+
+    expect(pokemon.nombre).toBe("bulbasaur");
+  });
+});
+
+describe("El panel muestra el panel de la derecha", () => {
+  it("Recoge bien la descripcion del pokemon", async () => {
+    const descripcion =
+      "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.";
+
+    const result = await funciones.obtenerPokemonDescripcion("1");
+
+    expect(result).toBe(descripcion);
+  });
+  it("Recoge bien las evoluciones del pokemon", () => {});
+  it("Recoge bien las devilidades del pokemon", () => {});
+});
