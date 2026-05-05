04c9b75e7175e639309ba41bfcc6dbab1dc1a15a
markelgomez-coder <markel.gomezr@gmail.com>
2026-05-05 10:03:44 +0200

    Test: fetch testeado

--- test/funciones-API.test.js
+++ test/funciones-API.test.js
@@ -3,12 +3,12 @@ import * as funcionesGenerales from "../dist/ts/funciones-generales.js";
 import * as funcionesAPI from "../dist/ts/funciones-API.js";
 
 describe("Hacer fetch", () => {
-  (it("El fetch se hace correctamente"),
-    () => {
+  it("El fetch se hace correctamente",
+    async () => {
       const url = "https://pokeapi.co/api/v2/pokemon/1";
-      const resultado = "";
-      const result = funcionesAPI.hacerFetch(url);
+      const resultado = "bulbasaur";
+      const result = await funcionesAPI.hacerFetch(url);
 
-      expect(result).toBe(resultado);
+      expect(result.name).toBe(resultado)
     });
 });
