2bfe453c3002d828b8a6a1d5b4a3f38f8a0c2a66
markelgomez-coder <markel.gomezr@gmail.com>
2026-05-08 09:56:28 +0200

    Test: Hecho el test para comprobar que se añaden bien los pokemon al dreamteam

--- test/funciones-generales.test.js
+++ test/funciones-generales.test.js
@@ -1,14 +1,36 @@
-import { describe, it, expect, beforeAll, beforeEach } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import * as funcionesGenerales from "../dist/ts/funciones-generales.js";
-import * as datosGenerales from "../dist/ts/datos-generales.js"
-import { obtenerPokemon } from "../dist/ts/funciones-API.js";
+import * as datosGenerales from "../dist/ts/datos-generales.js";
 
 describe("Cambiar la lista de los pokemons en el dreamTeam", () => {
-  it("Añadir un pokemon al dreamTeam", async () => {
-    const pokemon = await obtenerPokemon(1);
-    funcionesGenerales.sumarAlDreamTeam(pokemon)
-    expect(pokemon.name).toBe(undefined);
+  beforeEach(() => {
+    datosGenerales.dreamTeam.lenght = 0;
+    datosGenerales.dreamTeam.push({nombre: "charmander", numero: "004", tipos: ["fire"]});
+    datosGenerales.dreamTeam.push({nombre: "charmeleon", numero: "005", tipos: ["fire"]});
   });
+
+  it("Añadir un pokemon al dreamTeam", async () => {
+    const pokemon = {
+      nombre: "bulbasaur",
+      numero: 1,
+      imagen: "src",
+      tipos: ["grass", "poison"],
+      peso: 12,
+      altura: 7,
+      hp: 45,
+      atk: 45,
+      def: 45,
+      sat: 45,
+      sdf: 45,
+      spd: 45,
+      dream_team: false,
+    };
+
+    expect(datosGenerales.dreamTeam).not.toContain(pokemon);
+    funcionesGenerales.sumarAlDreamTeam(pokemon);
+    expect(datosGenerales.dreamTeam).toContain(pokemon);
+  });
+
   it("Quitar un pokemon del dreamTeam", () => {
     expect(1).toBe(1);
   });
